3eed2143faaec576cc67075fde1b0f41c9418439
Minji Bake <bigand32@gmail.com>
2019-05-14 10:42:05 +0900

    #2 메뉴 선택 추가 코드 구현

--- CB_Choice/Form1.cs
+++ CB_Choice/Form1.cs
@@ -11,9 +11,49 @@ namespace CB_Choice
 {
     public partial class Form1 : Form
     {
+        string[] SList = new string[]{"김밥", "샐러드김밥", "야채김밥",
+            "소고기김밥" ,"계란김밥", "라볶이"};
+        string orgStr = ""; // 선택 결과 저장
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < SList.Length; i++)
+            {
+                this.cbList.Items.Add(SList[i]);
+            }
+            orgStr = this.lblResult.Text;
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (this.txtList.Text != "")
+            {
+                this.cbList.Items.Add(this.txtList.Text);
+                MessageBox.Show("아이템을 추가하였습니다.", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("아이템을 입력해주세요.", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtList.Focus();
+            }
+            this.txtList.Text = "";
+
+        }
+
+        private void cbList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.cbList.Text != "")
+            {
+                this.lblResult.Text = orgStr + this.cbList.Text;
+            }
+
         }
     }
 }
